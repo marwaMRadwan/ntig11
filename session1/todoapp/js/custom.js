c180356549473a63057df234377b53a5641761b0
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-03 15:00:08 +0200

    s1

--- session1/todoapp/js/custom.js
+++ session1/todoapp/js/custom.js
@@ -1,5 +1,9 @@
 const task = document.querySelector('#task')
-const heads = ["taskTitle", "taskContent", "taskDetails"]
+const heads = [
+    {inForm:"taskTitle", inView:"Task Title"},
+    {inForm:"taskContent", inView:"Task Content"},
+    {inForm:"taskDetails", inView:"Task Details"}
+]
 
 saveDataToStorage = (data) =>{
     localStorage.setItem('myTasks', JSON.stringify(data))
@@ -22,7 +26,7 @@ if(task){
     task.addEventListener('submit', function(e){
         e.preventDefault()
         let task ={ }
-        heads.forEach( h => task[h]= this.elements[h].value )
+        heads.forEach( h => task[h.inForm]= this.elements[h.inForm].value )
         myTasks.push(task)
         saveDataToStorage(myTasks)
         this.reset()
@@ -31,6 +35,39 @@ if(task){
         },1000)
     })
     
+}
+
+const table = document.querySelector('#tableData')
+if(table){
+    let thead = document.createElement('thead')
+    table.appendChild(thead)
+    heads.forEach( h =>{
+        let th = document.createElement('th')
+        th.textContent = h.inView
+        thead.appendChild(th)    
+    })
+    tbody= document.createElement('tbody')
+    table.appendChild(tbody)
+    if(myTasks.length==0){  
+        tr = document.createElement('tr')
+        tbody.appendChild(tr)
+        td= document.createElement('td')
+        td.colSpan="3"
+        td.textContent= 'no data'
+        tr.appendChild(td)
+    }
+    else{
+        myTasks.forEach(task=>{
+            tr = document.createElement('tr')
+            tbody.appendChild(tr)
+            heads.forEach(h=>{
+                td= document.createElement('td')
+                td.textContent= task[h.inForm]
+                tr.appendChild(td)
+    
+            })
+        })
+    }
 }
 
 
