5049ae44b580b6afddabf200f07dd03b7051aea8
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-06 09:53:24 +0200

    s4

---
+++ session4/app.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const yargs = require('yargs')
+//read from file
+
+//write to file
+
+//add user yarg
+
+//add task to user yarg
+
+// delete task from user yarg
+
+//search user
+
+//show all users
+
+// show single user
+
+//edit user
+
+// delete user
+
+yargs.argv
