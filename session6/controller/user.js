8d5785f14a9633ee5cb9a18d015b09a8cc7508f4
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-10 13:20:50 +0200

    s6

--- session6/controller/user.js
+++ session6/controller/user.js
@@ -1,3 +1,26 @@
+const fs = require('fs')
+//read from file
+readData = () =>{
+    let data
+    try{
+        data = JSON.parse( fs.readFileSync('model/data.json'))
+        if(!Array.isArray(data)) throw new Error('data not an array')
+    }
+    catch(e){
+        data = []
+        console.log(`kan fe error fa 7atena array fady ${e}`)
+    }
+    return data
+}
+//write to file
+writeData = (allUsers) =>{
+    try{
+        fs.writeFileSync('model/data.json', JSON.stringify(allUsers))
+    }
+    catch(e){
+        console.log(`error in write to file ${e}`)
+    }
+}
 class UserController{
     static goAll(req,res){
         res.redirect('/all')
@@ -21,10 +44,17 @@ class UserController{
         res.render('edit', data)
     }
     static add(req, res){
-        let data = {
-            pageTitle: "Add new user"
-        }
+        let data = { pageTitle: "Add new user"}
+        if(req.query.userName){
+            let user = { id: Date.now(), name: req.query.userName, age: req.query.age }
+            let allUsers = readData()
+            allUsers.push(user)
+            writeData(allUsers)
+            res.redirect('/add')
+       }
+       else{
         res.render('add', data)
+       }
     }
     static del(req, res){
         res.send('deleted')
