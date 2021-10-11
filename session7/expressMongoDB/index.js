40144041b40f94c9abb8a9bbaa935a5bb2ecc0cc
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-11 13:07:14 +0200

    s7

---
+++ session7/expressMongoDB/index.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const app = require('./src/app')
+const PORT = process.env.PORT||3000
+app.get('*', (req,res)=> res.send('error loading url'))
+
+app.listen(PORT, ()=> console.log(`app on http://localhost:${PORT}`))
