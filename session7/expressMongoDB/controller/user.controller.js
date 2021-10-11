a5c19f22a0aea5728b490f34b767f6627b987f31
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-11 13:57:52 +0200

    s7

--- session7/expressMongoDB/controller/user.controller.js
+++ session7/expressMongoDB/controller/user.controller.js
@@ -1,4 +1,42 @@
+const db = require('../dbConnection/db')
+
 class User{
+    static addUser = (req,res)=>{
+        res.render('user/addPost')
+    }
+    static addUserData= (req,res)=>{
+        db((err, dbCilent)=>{
+            if(err) res.send('database error')
+            dbCilent.collection('user').insertOne(req.body)
+            .then(()=> res.redirect('/all'))
+            .catch(()=>res.send('error'))
+        })
+    }
+    static showAll = (req,res)=>{
+        db((err, dbCilent)=>{
+            if(err) res.send('database error')
+            dbCilent.collection('user').find().toArray((e, allUsers)=>{
+                if(e) res.send("fe error")
+                res.render('user/all', {
+                    allUsers, 
+                    userStatus: allUsers.length==0?false:true
+                })
+            })
+        })
+    }
+    static showSingle = (req,res)=>{
+        res.render('user/single')
+    }
+
+
+    static editUser = (req,res)=>{
+        res.render('user/edit')
+    }
+
+
+    static del = (req,res)=>{
+        res.send('deleted')
+    }
 
 }
 module.exports = User
--- session7/expressMongoDB/routes/user.routes.js
+++ session7/expressMongoDB/routes/user.routes.js
@@ -1,4 +1,14 @@
 const router = require('express').Router()
 const User = require('../controller/user.controller')
 
+router.get("/add", User.addUser)
+router.post("/add", User.addUserData)
+
+
+router.get("", User.showAll)
+router.get("/all", User.showAll)
+router.get("/single/:id", User.showSingle)
+router.get("/edit/:id", User.editUser)
+router.get("/delete/:id", User.del)
+
 module.exports = router
