6fe555b7cec9d4584d8233f6257baeba8efe0f53
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-13 10:32:28 +0200

    s9

---
+++ session9/src/app.js
@@ -0,0 +1,22 @@
+//run required scripts
+require('dotenv').config()
+require('../db/connection')
+
+//call required packages
+const express = require("express")
+const cors = require("cors")
+//create express intance
+const app = express()
+app.use(cors())
+//use json and urlencoded middleware 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//routes files 
+const userRoutes = require('../routes/user.routes')
+const postRoutes = require('../routes/post.routes')
+
+app.use('/user',userRoutes)  // 45 r   /user/x
+app.use('/post',postRoutes)  // 35 r => app.get('/x')  /post/x
+
+module.exports = app
