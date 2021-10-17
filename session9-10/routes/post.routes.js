dbcabb80519a32d1f065439a6ebfdd938d240c37
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-17 14:57:59 +0200

    s11

--- session9-10/routes/post.routes.js
+++
@@ -1,5 +0,0 @@
-const router = require('express').Router()
-
-router.get("/test", (req, res)=> res.send("test post"))
-
-module.exports = router
--- session9/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const UserController = require("../controller/user.controller")
-
-router.post("/register", UserController.register)
-router.patch("/activate/:id", UserController.activateUser)
-module.exports = router
