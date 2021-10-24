eceb0898602058667562f050811a79446a1a2cd3
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-24 10:46:10 +0200

    s15

---
+++ session15/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+//   title = '';
+//   val=5
+// i=0
+  //lifecycle hooks
+  // constructor(){console.log("constructor")}
+  // ngOnInit(){ console.log('ngOnInit')}
+  // ngOnChanges(){console.log('ngOnChanges')}
+  // ngDoCheck(){console.log('ngDoCheck')}
+  // ngAfterContentChecked(){
+  //   console.log('ngAfterContentChecked');
+  // }
+  // ngAfterViewChecked(){
+  //   this.i++
+  //   console.log('ngAfterViewChecked')
+  //   if(this.i>1)this.title="test"
+  // }
+  // ngAfterContentInit(){console.log('ngAfterContentInit')}
+  // ngAfterViewInit(){console.log('ngAfterViewInit'); }
+  // ngOnDestroy(){console.log('ngOnDestroy')}
+
+  // add(){ this.val++}
+  // sub(){this.val--}
+}
