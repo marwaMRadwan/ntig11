c834e2be3deaea41ff1603eec5b28ffe153e6726
marwaMRadwan <marwaradwan872@hotmail.com>
2021-10-24 14:45:35 +0200

    s15

--- session15/src/app/register2/register2.component.ts
+++ session15/src/app/register2/register2.component.ts
@@ -8,7 +8,7 @@ import { DataService } from '../services/data.service';
   styleUrls: ['./register2.component.css']
 })
 export class Register2Component implements OnInit {
-
+  msg=""
   user = {
     name:"",
     age:"",
@@ -24,10 +24,19 @@ export class Register2Component implements OnInit {
   onRegister(userData:NgForm){
     if(userData.valid){
       console.log(this.user)
-      this._data.register(this.user).subscribe(data=>{
-        console.log(data)
-      })
-      userData.resetForm()
+      this._data.register(this.user).subscribe(
+        (data)=>{ 
+this.msg= data.message
+        },
+        (e) => {
+          this.msg=e.error.message
+          console.log(e.error.message)
+        },
+        ()=>{
+          userData.resetForm()
+        }
+        )
+      
     }
   }
   test(x:any){
